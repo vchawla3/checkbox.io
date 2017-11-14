fee65c71907f87e3dd1ea0b78e46b6422ab090ee
vchawla3 <vchawla3@ncsu.edu>
2017-11-14 11:51:42 -0500

    add alert to server for canary

--- server-side/site/server.js
+++ server-side/site/server.js
@@ -1,4 +1,6 @@
 var express = require('express'),
+	//Manager = require("express-performance-monitor").Manager,
+	//toobusy = require('toobusy-js'),
         cors = require('cors'),
 	marqdown = require('./marqdown.js'),
 	//routes = require('./routes/designer.js'),
@@ -6,9 +8,62 @@ var express = require('express'),
 	//upload = require('./routes/upload.js'),
 	create = require('./routes/create.js'),
 	study = require('./routes/study.js'),
+	os = require('os'),
 	admin = require('./routes/admin.js');
 
 var app = express();
+
+// Create function to get CPU information
+function cpuTicksAcrossCores() 
+{
+  //Initialise sum of idle and time of cores and fetch CPU info
+  var totalIdle = 0, totalTick = 0;
+  var cpus = os.cpus();
+ 
+  //Loop through CPU cores
+  for(var i = 0, len = cpus.length; i < len; i++) 
+  {
+		//Select CPU core
+		var cpu = cpus[i];
+		//Total up the time in the cores tick
+		for(type in cpu.times) 
+		{
+			totalTick += cpu.times[type];
+		}     
+		//Total up the idle time of the core
+		totalIdle += cpu.times.idle;
+  }
+ 
+  //Return the average Idle and Tick times
+  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+}
+
+var startMeasure = cpuTicksAcrossCores();
+
+function cpuAverage()
+{
+	var endMeasure = cpuTicksAcrossCores(); 
+ 
+	//Calculate the difference in idle and total time between the measures
+	var idleDifference = endMeasure.idle - startMeasure.idle;
+	var totalDifference = endMeasure.total - startMeasure.total;
+ 
+	//Calculate the average percentage CPU usage
+	console.log(100 * ((totalDifference - idleDifference)/totalDifference));
+	return 100 * ((totalDifference - idleDifference)/totalDifference);
+}
+
+app.use(function(req, res, next) {
+  
+  // If cpu usage is currently above 15, return 503
+  //console.log("hello");
+  if (cpuAverage() > 15) res.send(503, "I'm busy right now, sorry.");
+  else next();
+});
+
+// toobusy.onLag(function(currentLag) {
+//   console.log("Event loop lag detected! Latency: " + currentLag + "ms");
+// });
 
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
@@ -24,6 +79,11 @@ var corsOptions = {
 };
 
 app.options('/api/study/vote/submit/', cors(corsOptions));
+
+//Status from monitor manager!!
+// app.get("/status", function(req, res) {
+//   res.json(req.monitor.manager.getState());
+// });
 
 app.post('/api/design/survey', 
 	function(req,res)
